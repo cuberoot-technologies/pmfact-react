45cdef272cee10a43fc6fbf636054db82bdb5448
bhargavi-cuberoot <bhargavi@advantedges.com>
2024-01-22 16:21:01 +0530

    feat. add dropdown to header

--- src/components/Textbar.jsx
+++ src/components/Textbar.jsx
@@ -1,58 +1,36 @@
-
-
 import React from "react";
 import { Link } from "react-router-dom";
-import Slider from "react-slick";
+import { Dropdown } from "react-bootstrap";
+
+const linkStyle = {
+  marginRight: '10px', 
+  textDecoration: 'none',
+  color: 'black', 
+  padding: '8px', 
+};
 
 const Textbar = () => {
-  var settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 2,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 2,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
   return (
     <>
       <div className="textslide">
-        <div className="container">
-          <Slider {...settings}>
-            <Link to="/">Home</Link>
-            <Link to="/parliament-performance">Parliament Performance</Link>
-            <Link to="/home-mp-performance">MPs Performance </Link>
-            <Link to="/rate-your-mp">Rate Your MPs </Link>{" "}
-            <Link to="/news-and-videos">News & Videos </Link>
-            <Link to="/public-voice">Public Voice </Link>
-            <Link to="/our-mps">Our MPs </Link>
-            <Link to="/our-expert">PF Experts </Link>
-            <Link to="/parliament-blog">Parliamentary blog</Link>
-            <Link to="/wish-your-mp">Wish your Mp</Link>
-          </Slider>
+        <div className="container d-flex justify-content-between">
+          <Link to="/" style={linkStyle}>Home</Link>
+          <Link to="/news-and-videos" style={linkStyle}>News & Videos</Link>
+          <Link to="/public-voice" style={linkStyle}>Public Voice</Link>
+          <Link to="/our-expert" style={linkStyle}>PF Experts</Link>
+          <Link to="/parliament-blog" style={linkStyle}>Parliamentary blog</Link>
+          <Dropdown>
+            <Dropdown.Toggle variant="link" id="dropdown-basic" style={{ ...linkStyle, fontSize: '18px'}}>
+              MPs Section
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item as={Link} to="/parliament-performance" style={linkStyle}>Parliament Performance</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/rate-your-mp" style={linkStyle}>Rate Your MPs</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/our-mps" style={linkStyle}>Our MPs</Dropdown.Item>
+              <Dropdown.Item as={Link} to="/wish-your-mp" style={linkStyle}>Wish your Mp</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </div>
       </div>
     </>
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -40,18 +40,6 @@ function Topbar() {
               />
             </div>
           </Navbar.Brand>
-
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              {/* <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link> */}
-            </Nav>
-          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
