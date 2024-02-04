1e70f723e397ebc91b46dc95e18458612934506d
bhargavi-cuberoot <bhargavi@advantedges.com>
2024-02-05 03:47:13 +0530

    style: add fontweight and other styling to textbarslider

--- src/components/textbarslider.jsx
+++ src/components/textbarslider.jsx
@@ -1,63 +1,73 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
 
 const SliderTextbar = () => {
-    var settings = {
-      dots: false,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 6,
-      slidesToScroll: 2,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 2,
-            infinite: true,
-          },
+  var settings = {
+    dots: false,
+    infinite: false,
+    arrows:false,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          infinite: true,
         },
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
         },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-          },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3.6,
+          slidesToScroll: 1,
         },
-      ],
-    };
+      },
+    ],
+  };
 
-    return (
-        <>
-        <div className="slider-textslider">
-<div className="textslide">
-<div className="container">
-  <Slider {...settings}>
-    <Link to="/">Home</Link>
-    <Link to="/parliament-performance">Parliament Performance</Link>
-    <Link to="/home-mp-performance">MPs Performance </Link>
-    <Link to="/rate-your-mp">Rate Your MPs </Link>{" "}
-    <Link to="/news-and-videos">News & Videos </Link>
-    <Link to="/public-voice">Public Voice </Link>
-    <Link to="/our-mps">Our MPs </Link>
-    <Link to="/our-expert">PF Experts </Link>
-    <Link to="/parliament-blog">Parliamentary blog</Link>
-    <Link to="/wish-your-mp">Wish your Mp</Link>
-  </Slider>
-  </div>
-  </div>
-  </div>
-        </>
-        );
-    };
-    
-    export default SliderTextbar;
+  const linkStyle = {
+    textDecoration: 'none',
+    padding: '4px',
+    marginRight: '8px',
+    paddingRight: '10px',
+    fontWeight: '700',
+    color: '#000',
+    display: 'inline-block',
+    borderRight: '1px solid #ccc',
+  };
+
+  return (
+    <div className="slider-textslider">
+      <div className="textslide">
+        <div className="container">
+          <Slider {...settings}>
+            <div><Link to="/" style={linkStyle}>Home</Link></div>
+            <div><Link to="/parliament-performance" style={linkStyle}>Parliament Performance</Link></div>
+            <div><Link to="/home-mp-performance" style={linkStyle}>MPs Performance</Link></div>
+            <div><Link to="/rate-your-mp" style={linkStyle}>Rate Your MPs</Link></div>
+            <div><Link to="/news-and-videos" style={linkStyle}>News & Videos</Link></div>
+            <div><Link to="/public-voice" style={linkStyle}>Public Voice</Link></div>
+            <div><Link to="/our-mps" style={linkStyle}>Our MPs</Link></div>
+            <div><Link to="/our-expert" style={linkStyle}>PF Experts</Link></div>
+            <div><Link to="/parliament-blog" style={linkStyle}>Parliamentary blog</Link></div>
+            <div><Link to="/wish-your-mp" style={linkStyle}>Wish your Mp</Link></div>
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SliderTextbar;
