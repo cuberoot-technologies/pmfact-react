4db42af721ef23dfa732b5748227f16502d4a3cb
bhargavi-cuberoot <bhargavi@advantedges.com>
2024-02-05 03:00:48 +0530

    add: search icon to texbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -125,7 +125,8 @@ function Topbar() {
         <i className="bi bi-facebook" style={{ marginRight: "15px" }}></i>
         <i className="bi bi-whatsapp" style={{ marginRight: "15px" }}></i>
         <i className="bi bi-linkedin" style={{ marginRight: "15px" }}></i>
-        <i className="bi bi-twitter-x" style={{ marginRight: "13px" }}></i>
+        <i className="bi bi-twitter-x" style={{ marginRight: "15px" }}></i>
+        <i class="bi bi-search" style={{marginRight: "13px" }}></i>
         </div>
       </div>
       </div>
