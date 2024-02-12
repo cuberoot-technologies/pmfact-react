51fc5769225c2ca30092d71f5c4939c7971c0cb8
bhargavi-cuberoot <bhargavi@advantedges.com>
2024-02-12 22:18:08 +0530

    add: add slider content

--- src/components/newsandvideo/newsandvideoshomepage/trending.jsx
+++ src/components/newsandvideo/newsandvideoshomepage/trending.jsx
@@ -81,7 +81,7 @@ const Trending = () => {
                 <div className="news-content-container">
                   <div className="news-image-container">
 
-                    {/* <Link to="/news-and-videos-category"> */}
+                    <Link to="/news-and-videos-category">
                     <div className="image-container">
                       <img
                         src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
@@ -95,61 +95,19 @@ const Trending = () => {
                         <span>12 jan 2023</span>
                       </div>
                     </div>
-                    {/* </Link> */}
+                    </Link>
                   </div>
                   <div className="text-news-container">
                     <div className="newsmainheading">
-                      <h2>Watch:10 days Later: What Italians Wish They Had Known</h2>
+                      <h3>वायरस आधुनिक इतिहास की सबसे गंभीर चुनौतियों में से एक के साथ नेताओं का सामना करता है</h3>
                     </div>
-                    <div className="catergory-box">Video</div>
-                    <div className="categorybox">
+                    <div className="catergory-box" style={{marginBottom:'0.5rem'}}>Video</div>
+                    <div className="categorybox" style={{marginBottom:'0.5rem'}}>
                       <p style={{ marginRight: '1rem' }}>Stephen Romero</p>
                       <p>19/03/2020-10:48</p>
                     </div>
                     <div className="categorytext">
-                      <p>A supermarket worker was spat at by a customer attempting to
-                        stockpile Pot Noodles while another was told: "I hope you you
-                        get the virus...</p>
-                    </div>
-                  </div>
-                  <div className="adverbox">
-                    <img src="https://placehold.co/200x200" alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className="breaking-news">
-                <div className="news-content-container">
-                  <div className="news-image-container">
-
-                    {/* <Link to="/news-and-videos-category"> */}
-                    <div className="image-container">
-                      <img
-                        src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
-                        alt=""
-                      />
-                      <div className="overlay"></div>
-                      <div className="caption-news">
-                        <p><BsPlay /></p>
-                        <h6>Entertainment</h6>
-                        <h1>Italy Lombardia reports first 2 corona virus deaths mind</h1>
-                        <span>12 jan 2023</span>
-                      </div>
-                    </div>
-                    {/* </Link> */}
-                  </div>
-                  <div className="text-news-container">
-                    <div className="newsmainheading">
-                      <h2>Watch:10 days Later: What Italians Wish They Had Known</h2>
-                    </div>
-                    <div className="catergory-box">Video</div>
-                    <div className="categorybox">
-                      <p style={{ marginRight: '1rem' }}>Stephen Romero</p>
-                      <p>19/03/2020-10:48</p>
-                    </div>
-                    <div className="categorytext">
-                      <p>A supermarket worker was spat at by a customer attempting to
-                        stockpile Pot Noodles while another was told: "I hope you you
-                        get the virus...</p>
+                      <p>पॉट नूडल्स इकट्ठा करने की कोशिश कर रहे एक ग्राहक ने एक सुपरमार्केट कर्मचारी पर थूक दिया था...</p>
                     </div>
                   </div>
                   <div className="adverbox">
@@ -162,7 +120,7 @@ const Trending = () => {
                 <div className="news-content-container">
                   <div className="news-image-container">
 
-                    {/* <Link to="/news-and-videos-category"> */}
+                    <Link to="/news-and-videos-category">
                     <div className="image-container">
                       <img
                         src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
@@ -176,21 +134,58 @@ const Trending = () => {
                         <span>12 jan 2023</span>
                       </div>
                     </div>
-                    {/* </Link> */}
+                    </Link>
                   </div>
                   <div className="text-news-container">
                     <div className="newsmainheading">
-                      <h2>Watch:10 days Later: What Italians Wish They Had Known</h2>
+                      <h3>वायरस आधुनिक इतिहास की सबसे गंभीर चुनौतियों में से एक के साथ नेताओं का सामना करता है</h3>
                     </div>
-                    <div className="catergory-box">Video</div>
-                    <div className="categorybox">
+                    <div className="catergory-box" style={{marginBottom:'0.5rem'}}>Video</div>
+                    <div className="categorybox" style={{marginBottom:'0.5rem'}}>
                       <p style={{ marginRight: '1rem' }}>Stephen Romero</p>
                       <p>19/03/2020-10:48</p>
                     </div>
                     <div className="categorytext">
-                      <p>A supermarket worker was spat at by a customer attempting to
-                        stockpile Pot Noodles while another was told: "I hope you you
-                        get the virus...</p>
+                      <p>पॉट नूडल्स इकट्ठा करने की कोशिश कर रहे एक ग्राहक ने एक सुपरमार्केट कर्मचारी पर थूक दिया था...</p>
+                    </div>
+                  </div>
+                  <div className="adverbox">
+                    <img src="https://placehold.co/200x200" alt="" />
+                  </div>
+                </div>
+              </div>
+
+              <div className="breaking-news">
+                <div className="news-content-container">
+                  <div className="news-image-container">
+
+                    <Link to="/news-and-videos-category">
+                    <div className="image-container">
+                      <img
+                        src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
+                        alt=""
+                      />
+                      <div className="overlay"></div>
+                      <div className="caption-news">
+                        <p><BsPlay /></p>
+                        <h6>Entertainment</h6>
+                        <h1>Italy Lombardia reports first 2 corona virus deaths mind</h1>
+                        <span>12 jan 2023</span>
+                      </div>
+                    </div>
+                    </Link>
+                  </div>
+                  <div className="text-news-container">
+                    <div className="newsmainheading">
+                      <h3>वायरस आधुनिक इतिहास की सबसे गंभीर चुनौतियों में से एक के साथ नेताओं का सामना करता है</h3>
+                    </div>
+                    <div className="catergory-box" style={{marginBottom:'0.5rem'}}>Video</div>
+                    <div className="categorybox" style={{marginBottom:'0.5rem'}}>
+                      <p style={{ marginRight: '1rem' }}>Stephen Romero</p>
+                      <p>19/03/2020-10:48</p>
+                    </div>
+                    <div className="categorytext">
+                      <p>पॉट नूडल्स इकट्ठा करने की कोशिश कर रहे एक ग्राहक ने एक सुपरमार्केट कर्मचारी पर थूक दिया था...</p>
                     </div>
                   </div>
                   <div className="adverbox">
